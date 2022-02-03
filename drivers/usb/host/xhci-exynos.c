0ctobot/neutrino_kernel_gs_raviole

commit 07a27dd0c13cd26b60b60a1c8132e5bf573bfa38

GKI: usb: host: partial revert of API changes missing in android-mainline
This is a partial revert of commit 1c63af79314f ("usb: host: enable SLEEP_USB
and SICD"). These additional API changes need to be upstreamed before pulling
into androi-mainline.

Bug: 199354250
Signed-off-by: Will McVicker <willmcvicker@google.com>
Change-Id: I685b61a08d7e6cd1e05a90cc670a4583a4e54204

drivers/usb/host/xhci-exynos.c
@@ -42,74 +42,7 @@ static const struct xhci_driver_overrides xhci_exynos_overrides __initconst = {
 	.extra_priv_size = sizeof(struct xhci_exynos_priv),
 	.reset = xhci_exynos_setup,
 	.start = xhci_exynos_start,
-	.address_device = xhci_exynos_address_device,
-	.bus_suspend = xhci_exynos_bus_suspend,
-	.bus_resume = xhci_exynos_bus_resume,
 };
-
-int xhci_exynos_address_device(struct usb_hcd *hcd, struct usb_device *udev)
-{
-	struct xhci_exynos_priv *priv = hcd_to_xhci_exynos_priv(hcd);
-	struct xhci_hcd_exynos *xhci_exynos = priv->xhci_exynos;
-	int ret;
-
-	ret = xhci_address_device(hcd, udev);
-	udev->dev.platform_data  = xhci_exynos;
-
-	return ret;
-}
-
-int xhci_exynos_bus_suspend(struct usb_hcd *hcd)
-{
-	struct xhci_exynos_priv *priv = hcd_to_xhci_exynos_priv(hcd);
-	struct xhci_hcd_exynos *xhci_exynos = priv->xhci_exynos;
-	struct xhci_hcd *xhci = hcd_to_xhci(hcd);
-	int ret, ret_phy, main_hcd;
-
-	if (hcd == xhci->main_hcd)
-		main_hcd = 1;
-	else
-		main_hcd = 0;
-
-	ret = xhci_bus_suspend(hcd);
-
-	if (hcd == xhci->main_hcd &&
-	    xhci_exynos->port_state == PORT_USB2) {
-		ret_phy = exynos_usbdrd_phy_vendor_set(xhci_exynos->phy_usb2, 1, 0);
-		if (ret_phy)
-			dev_info(xhci_exynos->dev, "phy vendor set fail\n");
-	}
-
-	xhci_exynos_wake_lock(xhci_exynos, main_hcd, 0);
-
-	return ret;
-}
-
-int xhci_exynos_bus_resume(struct usb_hcd *hcd)
-{
-	struct xhci_exynos_priv *priv = hcd_to_xhci_exynos_priv(hcd);
-	struct xhci_hcd_exynos *xhci_exynos = priv->xhci_exynos;
-	struct xhci_hcd *xhci = hcd_to_xhci(hcd);
-	int ret, main_hcd;
-
-	if (hcd == xhci->main_hcd)
-		main_hcd = 1;
-	else
-		main_hcd = 0;
-
-	if (hcd == xhci->main_hcd &&
-	    xhci_exynos->port_state == PORT_USB2) {
-		ret = exynos_usbdrd_phy_vendor_set(xhci_exynos->phy_usb2, 1, 1);
-		if (ret)
-			dev_info(xhci_exynos->dev, "phy vendor set fail\n");
-	}
-
-	ret = xhci_bus_resume(hcd);
-
-	xhci_exynos_wake_lock(xhci_exynos, main_hcd, 1);
-
-	return ret;
-}
 
 static void xhci_priv_exynos_start(struct usb_hcd *hcd)
 {
@@ -476,50 +409,6 @@ static void xhci_exynos_pm_runtime_init(struct device *dev)
 
 	dev->power.timer_expires = 0;
 	init_waitqueue_head(&dev->power.wait_queue);
-}
-
-int xhci_exynos_wake_lock(struct xhci_hcd_exynos *xhci_exynos,
-				   int is_main_hcd, int is_lock)
-{
-	struct usb_hcd	*hcd = xhci_exynos->hcd;
-	int idle_ip_index;
-	struct xhci_hcd *xhci = hcd_to_xhci(hcd);
-
-	dev_dbg(xhci_exynos->dev, "%s\n", __func__);
-
-	if (xhci->xhc_state & XHCI_STATE_REMOVING) {
-		dev_info(xhci_exynos->dev, "%s - Host removing return!\n",
-				__func__);
-		return -ESHUTDOWN;
-	}
-
-	if (is_lock) {
-		if (is_main_hcd) {
-			dev_info(xhci_exynos->dev, "%s: Main HCD WAKE LOCK\n", __func__);
-			__pm_stay_awake(xhci_exynos->main_wakelock);
-		} else {
-			dev_info(xhci_exynos->dev, "%s: Shared HCD WAKE LOCK\n", __func__);
-			__pm_stay_awake(xhci_exynos->shared_wakelock);
-		}
-		/* Add a routine for disable IDLEIP (IP idle) */
-		dev_info(xhci_exynos->dev, "IDLEIP(SICD) disable.\n");
-		idle_ip_index = dwc3_otg_get_idle_ip_index();
-		exynos_update_ip_idle_status(idle_ip_index, 0);
-	} else {
-		if (is_main_hcd) {
-			dev_info(xhci_exynos->dev, "%s: Main HCD WAKE UNLOCK\n", __func__);
-			__pm_relax(xhci_exynos->main_wakelock);
-		} else {
-			dev_info(xhci_exynos->dev, "%s: Shared HCD WAKE UNLOCK\n", __func__);
-			__pm_relax(xhci_exynos->shared_wakelock);
-		}
-
-		/* Add a routine for enable IDLEIP (IP idle) */
-		idle_ip_index = dwc3_otg_get_idle_ip_index();
-		exynos_update_ip_idle_status(idle_ip_index, 1);
-	}
-
-	return 0;
 }
 
 static int xhci_exynos_probe(struct platform_device *pdev)

drivers/usb/host/xhci-exynos.h
@@ -63,10 +63,5 @@ extern int exynos_usbdrd_phy_vendor_set(struct phy *phy, int is_enable,
 extern int dwc3_otg_get_idle_ip_index(void);
 void xhci_exynos_register_notify(void);
 void xhci_exynos_unregister_notify(void);
-static int xhci_exynos_address_device(struct usb_hcd *hcd, struct usb_device *udev);
-static int xhci_exynos_bus_suspend(struct usb_hcd *hcd);
-static int xhci_exynos_bus_resume(struct usb_hcd *hcd);
-static int xhci_exynos_wake_lock(struct xhci_hcd_exynos *xhci_exynos,
-				 int is_main_hcd, int is_lock);
 
 #endif	/* _XHCI_EXYNOS_H */

krazey/raviole-device

commit 64e536b73893f2de0821cfd67087d7fd6e4d9995

usb: host: Remove dead code causing a panic
Removing the USB notifier callback and supporting code because it's
causing a kernel panic when a USB-C to 3.5mm adapter is attached. This
is caused by the missing vendor hooks from android-mainline and
android13-5.15 for usb audio offload. The hooks need to be pushed
upstream before we can enable it here.

Bug: 217740293
Fixes: 07a27dd0c13c ("GKI: usb: host: partial revert of API changes missing in android-mainline")
Signed-off-by: Will McVicker <willmcvicker@google.com>
Change-Id: Ie6bdfa9db4421b87f610d4670af47c7949f31a4f

drivers/usb/host/xhci-exynos.c
@@ -104,229 +104,6 @@ static int xhci_exynos_start(struct usb_hcd *hcd)
 	return xhci_run(hcd);
 }
 
-void xhci_exynos_portsc_power_off(struct xhci_hcd_exynos *exynos, u32 on, u32 prt)
-{
-	struct xhci_hcd_exynos	*xhci_exynos = exynos;
-	void __iomem *portsc = xhci_exynos->usb3_portsc;
-
-	u32 reg;
-
-	spin_lock(&xhci_exynos->xhcioff_lock);
-
-	if (xhci_exynos->portsc_control_priority > prt) {
-		spin_unlock(&xhci_exynos->xhcioff_lock);
-		return;
-	}
-
-	xhci_exynos->portsc_control_priority = prt;
-
-	if (on && !xhci_exynos->port_off_done) {
-		dev_info(xhci_exynos->dev, "%s, Do not switch-on port\n", __func__);
-		spin_unlock(&xhci_exynos->xhcioff_lock);
-		return;
-	}
-
-	reg = readl(portsc);
-
-	if (on)
-		reg |= PORT_POWER;
-	else
-		reg &= ~PORT_POWER;
-
-	writel(reg, portsc);
-
-	reg = readl(phycon_base_addr + 0x70);
-	if (on)
-		reg &= ~DIS_RX_DETECT;
-	else
-		reg |= DIS_RX_DETECT;
-
-	writel(reg, phycon_base_addr + 0x70);
-
-	if (on)
-		xhci_exynos->port_off_done = 0;
-	else
-		xhci_exynos->port_off_done = 1;
-
-	spin_unlock(&xhci_exynos->xhcioff_lock);
-}
-
-int xhci_exynos_port_power_set(struct xhci_hcd_exynos *exynos, u32 on, u32 prt)
-{
-	struct xhci_hcd_exynos	*xhci_exynos = exynos;
-
-	if (xhci_exynos->usb3_portsc) {
-		xhci_exynos_portsc_power_off(xhci_exynos, on, prt);
-		return 0;
-	}
-
-	dev_info(xhci_exynos->dev, "%s, usb3_portsc is NULL\n", __func__);
-	return -EIO;
-}
-EXPORT_SYMBOL_GPL(xhci_exynos_port_power_set);
-
-static int xhci_exynos_check_port(struct xhci_hcd_exynos *exynos, struct usb_device *dev, bool on)
-{
-	struct usb_device *hdev;
-	struct usb_device *udev = dev;
-	struct usb_host_config *config;
-	struct usb_interface_descriptor *desc;
-	struct device *ddev = &udev->dev;
-	struct xhci_hcd_exynos	*xhci_exynos = exynos;
-	enum usb_port_state pre_state;
-	int usb3_hub_detect = 0;
-	int usb2_detect = 0;
-	int port, i;
-	int bInterfaceClass = 0;
-
-	if (udev->bus->root_hub == udev) {
-		dev_dbg(ddev, "this dev is a root hub\n");
-		goto skip;
-	}
-
-	pre_state = xhci_exynos->port_state;
-
-	/* Find root hub */
-	hdev = udev->parent;
-	if (!hdev)
-		goto skip;
-
-	hdev = dev->bus->root_hub;
-	if (!hdev)
-		goto skip;
-	dev_dbg(ddev, "root hub maxchild = %d\n", hdev->maxchild);
-
-	/* check all ports */
-	usb_hub_for_each_child(hdev, port, udev) {
-		dev_dbg(ddev, "%s, class = %d, speed = %d\n",
-			__func__, udev->descriptor.bDeviceClass,
-						udev->speed);
-		dev_dbg(ddev, "udev = 0x%8x, state = %d\n", udev, udev->state);
-		if (udev && udev->state == USB_STATE_CONFIGURED) {
-			if (!dev->config->interface[0])
-				continue;
-
-			bInterfaceClass	= udev->config->interface[0]
-					->cur_altsetting->desc.bInterfaceClass;
-			if (on) {
-				config = udev->config;
-				for (i = 0; i < config->desc.bNumInterfaces; i++) {
-					desc = &config->intf_cache[i]->altsetting->desc;
-					if (desc->bInterfaceClass == USB_CLASS_AUDIO) {
-						udev->do_remote_wakeup =
-							(udev->config->desc.bmAttributes &
-								USB_CONFIG_ATT_WAKEUP) ? 1 : 0;
-						if (udev->do_remote_wakeup == 1) {
-							device_init_wakeup(ddev, 1);
-							usb_enable_autosuspend(dev);
-						}
-						dev_dbg(ddev, "%s, remote_wakeup = %d\n",
-							__func__, udev->do_remote_wakeup);
-						break;
-					}
-				}
-			}
-			if (bInterfaceClass == USB_CLASS_HUB) {
-				xhci_exynos->port_state = PORT_HUB;
-				usb3_hub_detect = 1;
-				break;
-			} else if (bInterfaceClass == USB_CLASS_BILLBOARD) {
-				xhci_exynos->port_state = PORT_DP;
-				usb3_hub_detect = 1;
-				break;
-			}
-
-			if (udev->speed >= USB_SPEED_SUPER) {
-				xhci_exynos->port_state = PORT_USB3;
-				usb3_hub_detect = 1;
-				break;
-			} else {
-				xhci_exynos->port_state = PORT_USB2;
-				usb2_detect = 1;
-			}
-		} else {
-			dev_dbg(ddev, "not configured, state = %d\n", udev->state);
-		}
-	}
-
-	if (!usb3_hub_detect && !usb2_detect)
-		xhci_exynos->port_state = PORT_EMPTY;
-
-	dev_dbg(ddev, "%s %s state pre=%d now=%d\n", __func__,
-		on ? "on" : "off", pre_state, xhci_exynos->port_state);
-
-	return xhci_exynos->port_state;
-
-skip:
-	return -EINVAL;
-}
-
-static void xhci_exynos_set_port(struct usb_device *dev, bool on)
-{
-	struct xhci_hcd_exynos *xhci_exynos = dev_get_platdata(&dev->dev);
-	struct device *ddev = &dev->dev;
-	int check_port;
-
-	check_port = xhci_exynos_check_port(xhci_exynos, dev, on);
-	if (check_port < 0)
-		return;
-
-	switch (check_port) {
-	case PORT_EMPTY:
-		dev_dbg(ddev, "Port check empty\n");
-		xhci_exynos->is_otg_only = 1;
-		xhci_exynos_port_power_set(xhci_exynos, 1, 1);
-		break;
-	case PORT_USB2:
-		dev_dbg(ddev, "Port check usb2\n");
-		xhci_exynos->is_otg_only = 0;
-		xhci_exynos_port_power_set(xhci_exynos, 0, 1);
-		break;
-	case PORT_USB3:
-		xhci_exynos->is_otg_only = 0;
-		dev_dbg(ddev, "Port check usb3\n");
-		break;
-	case PORT_HUB:
-		dev_dbg(ddev, "Port check hub\n");
-		xhci_exynos->is_otg_only = 0;
-		break;
-	case PORT_DP:
-		dev_dbg(ddev, "Port check DP\n");
-		xhci_exynos->is_otg_only = 0;
-		break;
-	default:
-		break;
-	}
-}
-
-static int xhci_exynos_power_notify(struct notifier_block *self,
-			    unsigned long action, void *dev)
-{
-	switch (action) {
-	case USB_DEVICE_ADD:
-		xhci_exynos_set_port(dev, 1);
-		break;
-	case USB_DEVICE_REMOVE:
-		xhci_exynos_set_port(dev, 0);
-		break;
-	}
-	return NOTIFY_OK;
-}
-
-static struct notifier_block dev_nb = {
-	.notifier_call = xhci_exynos_power_notify,
-};
-
-void xhci_exynos_register_notify(void)
-{
-	usb_register_notify(&dev_nb);
-}
-
-void xhci_exynos_unregister_notify(void)
-{
-	usb_unregister_notify(&dev_nb);
-}
-
 static ssize_t
 xhci_exynos_ss_compliance_show(struct device *dev,
 			     struct device_attribute *attr,
@@ -515,8 +292,6 @@ static int xhci_exynos_probe(struct platform_device *pdev)
 	xhci_exynos->is_otg_only = 1;
 	xhci_exynos->port_state = PORT_EMPTY;
 
-	xhci_exynos_register_notify();
-
 	/*
 	 * Not all platforms have clks so it is not an error if the
 	 * clock do not exist.
@@ -673,8 +448,6 @@ static int xhci_exynos_remove(struct platform_device *dev)
 
 	__pm_relax(xhci_exynos->shared_wakelock);
 	wakeup_source_unregister(xhci_exynos->shared_wakelock);
-
-	xhci_exynos_unregister_notify();
 
 	if (!rhdev || !srhdev)
 		goto remove_hcd;