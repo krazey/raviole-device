krazey/raviole-device

commit db3bb2949e7c9a158e39599fe5c696c33adfab89

PT: stop execution if pt_handle_sysctl_register() fails
pt_handle_sysctl_register() may return -ENOMEM in case the allocation
fails. Stop the execution and return the error instead of assuming the
system works fine when out of memory. Change pt_handle_sysctl_register()
return type to int to propagate the error code.

This is a prerequisite patch for the replacement of the deprecated
register_sysctl_table().

Bug: 293569008
Change-Id: Id9d84a1c94bce4b02b2969138750aa6e0bdf3ce1
Signed-off-by: Tudor Ambarus <tudordana@google.com>

drivers/soc/google/pt/pt.c
@@ -11,6 +11,7 @@
 
 #include <soc/google/pt.h>
 #include "pt_trace.h"
+#include <linux/err.h>
 #include <linux/list.h>
 #include <linux/of_platform.h>
 #include <linux/module.h>
@@ -483,7 +484,7 @@ static void pt_handle_check(struct pt_handle *handle, int id)
 		panic("%s %s %d unknown\n", __func__, handle->node->name, id);
 }
 
-static void pt_handle_sysctl_register(struct pt_handle *handle)
+static int pt_handle_sysctl_register(struct pt_handle *handle)
 {
 	int id;
 	struct ctl_table *sysctl_table;
@@ -492,7 +493,7 @@ static void pt_handle_sysctl_register(struct pt_handle *handle)
 	sysctl_table = kmalloc_array(entry_cnt, sizeof(*sysctl_table),
 					GFP_KERNEL);
 	if (!sysctl_table)
-		return;
+		return -ENOMEM;
 	memset(sysctl_table, 0, sizeof(*sysctl_table) * entry_cnt);
 	sysctl_table[0].procname = "dev";
 	sysctl_table[0].mode = 0550;
@@ -524,6 +525,8 @@ static void pt_handle_sysctl_register(struct pt_handle *handle)
 		kfree(sysctl_table);
 	} else
 		handle->sysctl_table = sysctl_table;
+
+	return 0;
 }
 
 static void pt_handle_sysctl_unregister(struct pt_handle *handle)
@@ -777,7 +780,7 @@ void pt_client_unregister(struct pt_handle *handle)
 struct pt_handle *pt_client_register(struct device_node *node, void *data,
 	pt_resize_callback_t resize_callback)
 {
-	int id;
+	int id, ret;
 	unsigned long flags;
 	struct pt_handle *handle;
 	const char *propname = "pt_id";
@@ -803,11 +806,7 @@ struct pt_handle *pt_client_register(struct device_node *node, void *data,
 		const char *name;
 		struct pt_driver *driver;
 		int index;
-		int ret = of_property_read_string_index(node,
-						propname,
-						id,
-						&name);
-
+		ret = of_property_read_string_index(node, propname, id, &name);
 		if (ret != 0) {
 			kfree(handle);
 			return ERR_PTR(-ENOENT);
@@ -822,7 +821,9 @@ struct pt_handle *pt_client_register(struct device_node *node, void *data,
 		handle->pts[id].enabled = false;
 		handle->pts[id].ptid = PT_PTID_INVALID;
 	}
-	pt_handle_sysctl_register(handle);
+	ret = pt_handle_sysctl_register(handle);
+	if (ret)
+		return ERR_PTR(ret);
 
 	// Check if the node was not registered
 	spin_lock_irqsave(&pt_internal_data.sl, flags);

commit 7b665640e884611858dee9750a23250a8f84accb

PT: fix memleak in pt_client_register()
Fix memleak by freeing handle in pt_client_register().

Reported-by: Andre Draszik <draszik@google.com>
Fixes: db3bb2949 ("PT: stop execution if pt_handle_sysctl_register() fails")
Bug: 293569008
Change-Id: Ice252a993aa64fc1558288c3a780cf0286e29ffd
Signed-off-by: Tudor Ambarus <tudordana@google.com>

drivers/soc/google/pt/pt.c
@@ -846,8 +846,10 @@ struct pt_handle *pt_client_register(struct device_node *node, void *data,
 		handle->pts[id].ptid = PT_PTID_INVALID;
 	}
 	ret = pt_handle_sysctl_register(handle);
-	if (ret)
+	if (ret) {
+		kfree(handle);
 		return ERR_PTR(ret);
+	}
 
 	// Check if the node was not registered
 	spin_lock_irqsave(&pt_internal_data.sl, flags);